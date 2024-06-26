d50f103a43b57923d47ba159694df2a7485b5f60
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-06-26 11:42:11 +0200

    Lambda with frontend working. Starting Google reCAPTCHA implementation

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// document.getElementById('recordButton').addEventListener('click', function() {
-//     var button = document.getElementById('recordButton');
-//     var buttonText = document.getElementById('buttonText');
-//     var recordingIndicator = document.getElementById('recordingIndicator');
-
-//     if (buttonText.textContent === 'Grabar') {
-//         buttonText.textContent = 'Parar';
-//         button.classList.add('recording');
-//         recordingIndicator.style.display = 'flex';
-//         // Aquí inicia la grabación
-//     } else {
-//         buttonText.textContent = 'Grabar';
-//         button.classList.remove('recording');
-//         recordingIndicator.style.display = 'none';
-//         // Aquí para la grabación
-//     }
-// });
-
-// document.addEventListener('mousemove', function(e) {
-//     const x = e.clientX;
-//     const y = e.clientY;
-//     const size = 100; // Tamaño del rango alrededor del puntero
-
-//     document.body.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) ${size}px)`;
-// });
 
 document.addEventListener("DOMContentLoaded", function() {
     const text = "El Futuro del Aprendizaje Personalizado";
@@ -61,39 +36,39 @@ function recaptchaCallback(token) {
   submitBtn.disabled = false;
   submitBtn.style.cursor = 'pointer';
   submitBtn.style.backgroundColor = '#1e3745';
-  // Opcional: Puedes enviar el formulario automáticamente si lo deseas
+  
   // document.getElementById("myForm").submit();
 }
 
 document.getElementById("form_id").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita el envío del formulario para fines de demostración
-    // Aquí puedes agregar el código para enviar el formulario a tu servidor
-    // Si el envío del formulario es exitoso, muestra el mensaje de confirmación
-    const email = document.querySelector("input[name='email']").value.toLowerCase().replace(/\s/g, '');
+    event.preventDefault();
+
     const API = 'https://5zgbdl1m7g.execute-api.sa-east-1.amazonaws.com/save_email';
     
-    console.log(email);
-    console.log("Correo enviado...");
-    document.getElementById("confirmationModal").style.display = "block";
-    document.querySelector("input[name='email']").value = ""; // Limpiar el campo de correo
-    grecaptcha.reset(); // Reiniciar reCAPTCHA
-    
+    const email = document.querySelector("input[name='email']").value.toLowerCase().replace(/\s/g, '');
     const submitBtn = document.getElementById('submitBtn');
+    const responseMessage = document.getElementById("responseMessage");
+    
+    submitBtn.textContent = "Enviando...";
     submitBtn.disabled = true;
     submitBtn.style.cursor = 'not-allowed';
     submitBtn.style.backgroundColor = '#ccc';
+
+    document.querySelector("input[name='email']").value = "";
+    grecaptcha.reset();
 
     fetch(API, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ email: email })
+      body: JSON.stringify({ email: email, token: grecaptcha.getResponse() })
     })
     .then(response => response.json())
     .then(data => {
-      console.log('Sucess1:', data);
-      alert(data);
+      responseMessage.textContent = data.body;
+      document.getElementById("confirmationModal").style.display = "block";
+      submitBtn.textContent = "Enviar";
     })
     .catch((error) => {
       console.error('Error', error);
@@ -113,24 +88,3 @@ window.onclick = function(event) {
         document.getElementById("confirmationModal").style.display = "none";
     }
 };
-
-// document.getElementById('form_id').addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     const email = event.target.email.value;
-//     const recaptchaResponse = grecaptcha.getResponse();
-    
-//     fetch('https://baodlvjrsd.execute-api.sa-east-1.amazonaws.com/test', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({ email: email, recaptchaResponse: recaptchaResponse }),
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         alert(data.message);
-//     })
-//     .catch((error) => {
-//         console.error('Error:', error);
-//     });
-// }); 
